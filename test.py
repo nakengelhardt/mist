236e16d38061b6937d386c6ad616538ca06ce471
Sebastien Bourdeauducq <sebastien@milkymist.org>
2013-08-14 11:12:27 +0200

    test: improve signal naming

--- test.py
+++ test.py
@@ -11,17 +11,23 @@ class Test(Module):
 			If(btn3, led2.eq(btn1 | btn2)).Else(led2.eq(btn1 & btn2))
 		]
 
-m1 = m1.Platform()
+def main():
+	plat = m1.Platform()
 
-btn1, btn2, btn3 = (m1.request("user_btn") for i in range(3))
-led1, led2 = (m1.request("user_led") for i in range(2))
+	btn1 = plat.request("user_btn")
+	btn2 = plat.request("user_btn")
+	btn3 = plat.request("user_btn")
+	led1 = plat.request("user_led")
+	led2 = plat.request("user_led")
 
-t = Test(btn1, btn2, btn3, led1, led2)
+	t = Test(btn1, btn2, btn3, led1, led2)
 
-#f = t.get_fragment()
-#m1.finalize(f)
-#mist.synthesize(f, m1.constraint_manager.get_io_signals())
-#v_src, named_sc, named_pc = m1.get_verilog(f)
-#print(v_src)
+	# f = t.get_fragment()
+	# plat.finalize(f)
+	# mist.synthesize(f, plat.constraint_manager.get_io_signals())
+	# v_src, named_sc, named_pc = plat.get_verilog(f)
+	# print(v_src)
 
-m1.build(t, mode="mist")
+	plat.build(t, mode="mist")
+
+main()
